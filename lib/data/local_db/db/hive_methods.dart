1869b45eb53428a4cef93bf4bc0150656a522437
jean cuervo <martoranito@gmail.com>
2021-08-08 21:36:20 +0200

    implemented Hive DB

--- lib/data/local_db/db/hive_methods.dart
+++ lib/data/local_db/db/hive_methods.dart
@@ -1,36 +1,68 @@
+import 'dart:io';
+
+import 'package:hive/hive.dart';
 import 'package:skype_clone/data/local_db/interface/log_interface.dart';
 import 'package:skype_clone/data/models/log.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HiveMethods implements LogInterface {
+  String hiveBox = 'Call_Logs';
+
   @override
-  addLogs(LogModel log) {
-    print('adding values tu Hive DB');
-    // TODO: implement addLogs
-    throw UnimplementedError();
+  Future<void> init() async {
+    final Directory dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
   }
 
   @override
-  LogInterface close() {
-    // TODO: implement close
-    throw UnimplementedError();
+  Future<int> addLogs(LogModel log) async {
+    final Box<Map<String, dynamic>> box =
+        await Hive.openBox<Map<String, dynamic>>(hiveBox);
+
+    final Map<String, dynamic> logMap = log.toMap(log);
+
+    final int idOfInput = await box.add(logMap);
+
+    close();
+
+    return idOfInput;
+  }
+
+  Future<void> updateLogs(int index, LogModel newLog) async {
+    final Box<Map<String, dynamic>> box =
+        await Hive.openBox<Map<String, dynamic>>(hiveBox);
+
+    final Map<String, dynamic> newLogMap = newLog.toMap(newLog);
+
+    box.putAt(index, newLogMap);
+
+    close();
   }
 
   @override
-  deleteLog(int logId) {
-    // TODO: implement deleteLog
-    throw UnimplementedError();
+  Future<List<LogModel>> getLogs() async {
+    final Box<Map<String, dynamic>> box =
+        await Hive.openBox<Map<String, dynamic>>(hiveBox);
+
+    final List<LogModel> logList = <LogModel>[];
+
+    for (int i = 0; i < box.length; i++) {
+      final Map<String, dynamic>? logMap = box.getAt(i);
+
+      logList.add(LogModel.fromMap(logMap!));
+    }
+
+    return logList;
   }
 
   @override
-  Future<List<LogModel>> getLogs() {
-    // TODO: implement getLogs
-    throw UnimplementedError();
+  Future<void> deleteLog(int logId) async {
+    final Box<Map<String, dynamic>> box =
+        await Hive.openBox<Map<String, dynamic>>(hiveBox);
+
+    await box.deleteAt(logId);
   }
 
   @override
-  LogInterface? init() {
-    // TODO: implement init
-    print('initialized hive database');
-    return null;
-  }
+  void close() => Hive.close();
 }
